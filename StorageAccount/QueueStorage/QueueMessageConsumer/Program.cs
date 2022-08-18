7d6b6c13e31b21cde9f201a2438da9d1308a6cc6
Jakub Kozera <kozerakuba@gmail.com>
2022-08-18 20:28:31 +0200

    2.2 Queue storage + restructure solution

---
+++ StorageAccount/QueueStorage/QueueMessageConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Queues;
+using QueueMessageConsumer;
+
+Console.WriteLine("Hello, World!");
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=projectname1sane;AccountKey=Z7BDYxJqQgFKvJPpw7NcKPpURQkOraIPpO74njujqaWgVKBrs/qP4eHgp9r4taV1l/xQk00I4pFa+AStyjfaMw==;EndpointSuffix=core.windows.net";
+
+var queueName = "returns";
+
+QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+while (true)
+{
+    var message = queueClient.ReceiveMessage();
+    if (message.Value != null)
+    {
+        var dto = message.Value.Body.ToObjectFromJson<ReturnDto>();
+        Process(dto);
+        await queueClient.DeleteMessageAsync(message.Value.MessageId, message.Value.PopReceipt);
+    }
+
+    await Task.Delay(1000);
+}
+
+void Process(ReturnDto dto)
+{
+    Console.WriteLine($"Processing return with id: {dto.Id}, " +
+        $"for user : {dto.User}" +
+        $"from address: {dto.Address}");
+}
---
+++ StorageAccount/QueueStorage/QueueMessagePublisher/Controllers/QueueStorageController.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Mvc;
+using QueueMessagePublisher.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace QueueMessagePublisher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QueueStorageController : ControllerBase
+    {
+        [HttpPost("publish")]
+        public async Task<IActionResult> Publish(ReturnDto returnDto)
+        {
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=projectname1sane;AccountKey=Z7BDYxJqQgFKvJPpw7NcKPpURQkOraIPpO74njujqaWgVKBrs/qP4eHgp9r4taV1l/xQk00I4pFa+AStyjfaMw==;EndpointSuffix=core.windows.net";
+
+            var queueName = "returns";
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            await queueClient.CreateIfNotExistsAsync();
+
+            var serializedMessage = JsonSerializer.Serialize(returnDto);
+
+            await queueClient.SendMessageAsync(serializedMessage);
+
+            return Ok();
+        }
+    }
+}
