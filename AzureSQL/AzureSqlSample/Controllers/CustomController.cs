b286113e40801410c990dbce9a250972e0d8419b
Jakub Kozera <kozerakuba@gmail.com>
2022-08-23 20:27:55 +0200

    2.6 AzureSQL

---
+++ AzureSQL/AzureSqlSample/Controllers/CustomController.cs
@@ -0,0 +1,32 @@
+﻿using AzureSqlSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSqlSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomController : ControllerBase
+    {
+        private readonly ProjectNameDbContext _dbContext;
+
+        public CustomController(ProjectNameDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var customers = _dbContext.Customers
+                .Take(50)
+                .ToList();
+
+            return Ok(customers);
+        }
+    }
+}
