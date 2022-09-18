06a39cd24815af00061269ce2893a42fc00fd4d5
Jakub Kozera <jakub.kozera@inovirtue.com>
2022-09-18 09:34:42 +0200

    3.6 Azure Web API with Swagger

--- AzureFunctions/BlobTriggerIsolated/Program.cs
+++ AzureFunctions/BlobTriggerIsolated/Program.cs
@@ -1,7 +1,15 @@
+using Isolated;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<ToDoStore>();
+    })
+    .ConfigureOpenApi()
     .Build();
 
 host.Run();
---
+++ AzureFunctions/BlobTriggerIsolated/ToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace Isolated
+{
+    public class ToDo
+    {
+        public Guid Id { get; set; }
+        public string? Description { get; set; }
+        public bool IsFinished { get; set; }
+    }
+}
