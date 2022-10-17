bfaa0088751b6ec4d42e68ca74eaaed7939eacd4
Jakub Kozera <jakub.kozera@inovirtue.com>
2022-10-17 11:00:31 +0200

    Send gird

---
+++ Utilities/SendGrid/Program.cs
@@ -0,0 +1,25 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+Console.WriteLine("Hello world");
+
+var apiKey = "SG.api...key";
+
+var client = new SendGridClient(apiKey);
+
+var to = new EmailAddress("odbiorca.sendgrid@test.pl", "Odbiorca");
+var from = new EmailAddress("nadawca.sendgrid@test.pl", "Nadawca");
+
+var message = MailHelper.CreateSingleTemplateEmail(from, to, "d-34012b901ee64c45b400aeae06bb6d73", new
+{
+    Title = "Test title",
+    Description = "Some description"
+});
+
+var response = await client.SendEmailAsync(message);
+
+var responseBody = await response.Body.ReadAsStringAsync();
+
+Console.WriteLine("Email sent");
+Console.WriteLine(response.IsSuccessStatusCode);
+Console.WriteLine(responseBody);
